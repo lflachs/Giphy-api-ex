9b56278e52f646511b767fdd7c5543753901baa7
Leonard <leonard.flachs@gmail.com>
2020-02-24 14:47:52 +0100

    removed Elephant api + back

--- src/App.js
+++ src/App.js
@@ -16,28 +16,15 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    var myInit = {
-      method: "GET",
-      mode: "cors",
-      cache: "default"
-    };
     this.handleLoadGifs(this.state.query);
-    fetch(
-      "https://cors-anywhere.herokuapp.com/https://elephant-api.herokuapp.com/elephants",
-      myInit
-    )
-      .then(res => res.json())
-      .then(results => {
-        this.setState({ elephant: results });
-        console.log(this.state.elephant[0].image);
-      });
   }
 
   // function that load the gifs
   handleLoadGifs = query => {
+    const apiKey = "OD3oGfqxgGh8SZtwK6u0XHQtkSQef8N4";
     console.log("load gifs");
     fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_GIPHY}&q=${query}&limit=10`
+      `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10`
     )
       .then(res => res.json())
       .then(results => {
@@ -64,12 +51,7 @@ class App extends React.Component {
     return (
       <div>
         <header className="main-header">
-          <img
-            className="logo"
-            src={logo}
-            alt="logo"
-            onClick={() => this.handleLoadGifs("hello")}
-          />
+          <img className="logo" src={logo} alt="logo" />
           <SearchBar
             onInputChange={this.onInputChange}
             onSearchClick={this.onSearchClick}
